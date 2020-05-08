6bdfbfeea61428a8f1af63d958538b0444409ad7
Matheus Lemos <matheuslemosf@protonmail.com>
2020-05-08 15:22:12 -0300

    Updates

---
+++ precache-manifest.84c110f0d3b9f95a02c6404d79f17c60.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60c12476c3a668f16daafa6dc02442e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "041e40cc2734dd6a8e27",
+    "url": "/static/css/main.89d2c3ed.chunk.css"
+  },
+  {
+    "revision": "5cb069d77867cde24fdb",
+    "url": "/static/js/2.c66cb618.chunk.js"
+  },
+  {
+    "revision": "b3924902b6517db7f2b56facf8ba237b",
+    "url": "/static/js/2.c66cb618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041e40cc2734dd6a8e27",
+    "url": "/static/js/main.323303df.chunk.js"
+  },
+  {
+    "revision": "cac8951ea1ea72b675b3",
+    "url": "/static/js/runtime-main.390e1b7a.js"
+  },
+  {
+    "revision": "f550333abdc12f3450d2c022fe2a7317",
+    "url": "/static/media/background2.f550333a.jpg"
+  },
+  {
+    "revision": "26a53d52598536dc7bec48101ba564c2",
+    "url": "/static/media/background3.26a53d52.jpg"
+  },
+  {
+    "revision": "c059eefc72b79ccf2111b79ee8585c24",
+    "url": "/static/media/profile.c059eefc.jpg"
+  }
+]);
