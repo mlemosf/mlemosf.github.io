47037cadacb1989f803a9ebfce6623e15880ea4e
Matheus Lemos <matheuslemosf@protonmail.com>
2020-05-08 14:32:48 -0300

    Updated: Campo de experiência modificado
    Campo 'Experiência' modificado com as novas configuracões;
    Adicionadas novas imagens utilizadas no site.

--- src/App.js
+++ src/App.js
@@ -12,13 +12,21 @@ var sections = [
 		content: 'Sou Matheus, desenvolvedor de software e entusiasta dos vários\
 		movimentos e tecnologias neste vasto mundo da tecnologia. Já fiz projetos \
 		em Machine Learning, APIs REST e sou fascinado pelas novas tecnologias de \
-		arquitetura em numem, como Docker e Kubernetes. Estou sempre fazendo projetos \
+		arquitetura em nuvem, como Docker e Kubernetes. Estou sempre fazendo projetos \
 		dentro e fora de empregos, e falo com tranquilidade que computacão é a paixão \
 		da minha vida.'	
 	},
 	{
 		title: 'Experiência',
-		content: 'Tenho 2 anos de experiência desenvolvendo software backend'	
+		content: 'Tenho mais de 2 anos de experiência desenvolvendo software backend.\
+		De janeiro de 2018 a janeiro de 2020 trabalhei em uma startup desenvolvedo \
+		software voltado a distribuicão e alta disponibilidade de bancos de dados \
+		PostgreSQL e software relacionado ao desenvolvimento de modelos de \
+		Machine Learning. Também fui responsável por APIs REST para acesso aos dados. \
+		Em abril de 2020 tive experiências com desenvolvimento freelancer, desenvolvendo \
+		módulos para uma API REST. Nesses dois anos tive muita experiência em \
+		desenvolvimento backend em NodeJS e Python, utilizando Express e Flask como \
+		ferramentas principais.'	
 	}
 ]
 
